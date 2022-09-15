9d7aff4990050a0ea4ad3dfe6d3330b5c7d12733
Ingenious <ingeniousabhinav@gmail.com>
2022-09-15 14:25:33 +0530

    Another Push

--- sarangpurapp/settings.py
+++ sarangpurapp/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = 'django-insecure-76h7oo9cm^^)k$!**b(u1u4)wqxv8&=5hab7@d!-+my^+mkbh_
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', 'sarangpurnews.herokuapp.com']
 
 
 # Application definition
