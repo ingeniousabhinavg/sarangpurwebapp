ce1279ac5a5a3895261aa85560dde9e34d710cd0
Ingenious <ingeniousabhinav@gmail.com>
2022-09-15 19:34:58 +0530

    Final for Deployment

--- sarangpur/urls.py
+++ sarangpur/urls.py
@@ -5,7 +5,6 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('about/', views.about, name='about'),
     path('news/', views.news, name='news'),
-    #
     path('newss/', views.PostList.as_view(), name='index'),
     path('<slug:slug>/', views.PostDetail.as_view(), name='post_detail'),
 ]
--- sarangpurapp/urls.py
+++ sarangpurapp/urls.py
@@ -19,8 +19,8 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('', include('sarangpur.urls')),
     path('admin/', admin.site.urls),
+    path('', include('sarangpur.urls')),
 ]
 
 if settings.DEBUG:     
